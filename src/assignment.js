71c2b7d178a499d62a252852b2b0e3d84236ceb1
OyeneyinTosin <105616628+OyeneyinTosin@users.noreply.github.com>
2022-07-15 21:21:33 +0000

    check challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -13,26 +13,56 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+//solution to challenge 1
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for(let i = 0; i < arrayOfNumbers.length; i++) {
+    const numberInArray = arrayOfNumbers[i];
+    sum = sum + numberInArray;
+  }
+console.log("sum of numbers ==", sum)
 }
+let numbers = [68,-68,27,94,72,-25,-51,32,10,64,-94,4,34,-86,90,81,20,-56,-91,-50]
+sumOfNumbers(numbers)
+
 // assignment.sumOfNumbers = sumOfNumbers;
 
-/**
- * Challenge - 2
+
+ /**
+Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function
+
+    
  * 
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+  @returns number the count of the number
 
-/**
+ assignment.countEvenNumbers = countEvenNumbers;
+*/
+
+//solution to challenge-2
+
+function countEvenNumbers(arrayOfNumbers) {
+  let evenNumbers = [];
+
+  for (let i = 0; i<arrayOfNumbers.length; i++) {
+
+
+    if(arrayOfNumbers[i] % 2 ==0) {
+         evenNumbers.push(arrayOfNumbers[i]);
+      }
+
+  }
+  console.log("EvenNumbers count:",evenNumbers)
+}
+listOfNumbers = [17,0,67,41,49,21,1,86,76,2,54,14,93,84,14,77,35,7,29,64]
+countEvenNumbers(listOfNumbers)
+
+
+/*
  * Challenge - 3
  * 
  * Given an array of numbers representing temperatures in Celsius,
